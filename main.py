55286d5697120a0f9e65f9635d07f6fbfe9ababf
TuanKhaiNguyen1205 <tuankhainguyenn@gmail.com>
2026-05-26 21:30:16 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from load_data import *
 from preprocessing import *
 from visualization import *
+from feature_extraction import *
 
 
 def main():
@@ -21,43 +22,35 @@ def main():
     print("REMOVE MISSING VALUES & DUPLICATES")
     df = clean_missing_duplicate(df)
 
+    # CREATE REVIEW LENGTH
     df['Review_Length'] = df['Review Text'].apply(lambda x: len(str(x).split()))
     print("Created column: Review_Length")
 
     # VISUALIZATION
     print("\n--- VISUALIZATION ---")
-
-    ## Rating distribution
     visualize_rating_distribution(df)
-
-    ## Review length distribution
     visualize_review_length(df)
-
-    ## Average review length by rating
     visualize_average_length(df)
-
-    ## Heatmap
     visualize_heatmap(df)
-
-    ## WordCloud
     generate_wordcloud(df)
 
-    # PREPROCESSING
+    # TEXT PREPROCESSING
     print("TEXT PREPROCESSING")
     df = clean_text(df, 'Review Text')
 
-    ## POSITIVE VS NEGATIVE WORDCLOUD
+    # TOP WORDS VISUALIZATION
     print("TOP WORDS VISUALIZATION")
     top_words_visualization(df)
-    ## WordCloud Positive vs Negative (Top 20)
     top_words_positive_negative(df)
 
-    # FINAL DATASET
+    # FINAL CLEAN DATASET
     print("\n--- FINAL CLEAN DATASET ---")
     clean_df = create_final_dataset(df)
     print(clean_df.head())
 
-    
+    # FEATURE EXTRACTION
+    X_train, X_test, y_train, y_test, tfidf_vectorizer, word2vec_model, model_df = feature_extraction_pipeline(df)
+
 
 if __name__ == "__main__":
     main()
